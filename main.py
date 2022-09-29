e07ea20b2c4a2eee994e187dae53b965e2f32a14
Hlibisev <hlibisev@gmail.com>
2022-09-29 12:09:53 +0300

    black this

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from objects import Object
 
 
 def reflect(N, L):
-    return L - 2 * np.dot(L, N) * N 
+    return L - 2 * np.dot(L, N) * N
 
 
 def simple_raycast(orig, dir, objects: List[Object], depth):
@@ -18,50 +18,56 @@ def simple_raycast(orig, dir, objects: List[Object], depth):
 
     neirest_object = None
     min_dist = np.infty
-    
+
     for i, object in enumerate(objects):
         _, dist = object.ray_intersect(orig, dir)
-        
+
         if dist < min_dist:
             index = i
             min_dist = dist
             neirest_object = object
-    
-    if neirest_object is not None: 
+
+    if neirest_object is not None:
         point = orig + dir * min_dist
-        
+
         N = neirest_object.normal(point)
-        
+
         intensity = 0
         specular_intensity = 0
-        
+
         for light in LIGHTS:
             light_dir = light.position - point
             light_dir /= norm(light_dir)
 
             shadow_orig = point - 1e-3 * N if np.dot(light_dir, N) < 0 else point + 1e-3 * N
-                     
+
             if any(object.ray_intersect(shadow_orig, light_dir)[0] for k, object in enumerate(objects) if k != index):
                 continue
 
-            intensity += max(0, np.dot(light_dir, N)) * light.intensity 
-            specular_intensity += max(0, np.dot(reflect(N, light_dir), dir)) ** \
-                               neirest_object.material.specular_exponent * light.intensity 
+            intensity += max(0, np.dot(light_dir, N)) * light.intensity
+            specular_intensity += (
+                max(0, np.dot(reflect(N, light_dir), dir)) ** neirest_object.material.specular_exponent
+                * light.intensity
+            )
 
-        color = neirest_object.material.albedo_color * light.intensity * 0.2 + \
-                neirest_object.material.diffuse_color * intensity * 0.7 + \
-                neirest_object.material.specular_color * specular_intensity * 0.9
+        color = (
+            neirest_object.material.albedo_color * light.intensity * 0.2
+            + neirest_object.material.diffuse_color * intensity * 0.7
+            + neirest_object.material.specular_color * specular_intensity * 0.9
+        )
 
         reflect_dir = reflect(N, dir)
         reflect_orig = point - N * 1e-4 if np.dot(reflect_dir, N) < 0 else point + N * 1e-4
-        color += simple_raycast(reflect_orig, reflect_dir, objects, depth=depth+1) * neirest_object.material.reflection
-        
+        color += (
+            simple_raycast(reflect_orig, reflect_dir, objects, depth=depth + 1) * neirest_object.material.reflection
+        )
+
         return color
-    
+
     return np.zeros(3) + [0.1, 0.5, 0.9]
 
 
-def main(fov=np.pi/2):
+def main(fov=np.pi / 2):
     n = 1000
     image = np.zeros((n, n, 3))
 
@@ -71,32 +77,17 @@ def main(fov=np.pi/2):
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             orig = np.zeros(3)
-            
+
             x = (j / image.shape[1] - 0.5) * widght_screen
             y = -(i / image.shape[0] - 0.5) * height_screen
-            
+
             dir = np.array([x, y, -1])
             dir /= norm(dir)
 
             image[i, j] = simple_raycast(orig, dir, OBJECTS, 0) * 255
-    
+
     cv2.imwrite("img.png", image[..., ::-1])
-
-
-
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
--- objects.py
+++ objects.py
@@ -11,9 +11,11 @@ class Object(Protocol):
     material: Material
     reflection: float
 
-    def ray_intersect(self) -> List[int]: ...
+    def ray_intersect(self) -> List[int]:
+        ...
 
-    def normal(self, point) -> np.ndarray: ...
+    def normal(self, point) -> np.ndarray:
+        ...
 
 
 @dataclass
@@ -23,26 +25,26 @@ class Sphere:
     material: Material
 
     def ray_intersect(self, orig, dir):
-        """ https://www.ctralie.com/PrincetonUGRAD/Projects/COS426/Assignment3/part1.html#raysphere """
+        """https://www.ctralie.com/PrincetonUGRAD/Projects/COS426/Assignment3/part1.html#raysphere"""
         L = self.position - orig
-        
+
         a = np.dot(L, dir)
         c2 = np.dot(L, L)
-        h2 = c2 - a ** 2
+        h2 = c2 - a**2
 
-        if h2 > self.radius ** 2:
+        if h2 > self.radius**2:
             return False, np.infty
 
-        half_a_inside_sphere = np.sqrt(self.radius ** 2 - h2)
-        
+        half_a_inside_sphere = np.sqrt(self.radius**2 - h2)
+
         if a - half_a_inside_sphere > 0:
             return True, a - half_a_inside_sphere
 
         if a + half_a_inside_sphere > 0:
             return True, a + half_a_inside_sphere
-        
+
         return False, np.infty
-    
+
     def normal(self, point):
         "point must belong object"
         N = point - self.position
@@ -51,23 +53,24 @@ class Sphere:
         return N
 
 
-
 @dataclass
 class Plane:
-    def __init__(self, points: List[np.ndarray], material: Material, bounds = np.array([[-5, 5], [-5, 5], [-20, -5]])) -> None:
+    def __init__(
+        self, points: List[np.ndarray], material: Material, bounds=np.array([[-5, 5], [-5, 5], [-20, -5]])
+    ) -> None:
         self.points = points
         self.material = material
         self.bounds = bounds
         self.lin_coeff, self.constant = self.get_coeffitient(points)
 
     def ray_intersect(self, orig, dir):
-        """ hand made algorithm: 
-            trace = orig + t * dir, 
-            place: a * x + b * y + c * z + d = 0
+        """hand made algorithm:
+        trace = orig + t * dir,
+        place: a * x + b * y + c * z + d = 0
         """
 
         coef_t = np.dot(self.lin_coeff, dir)
-        constant_t = - self.constant - np.dot(self.lin_coeff, orig)
+        constant_t = -self.constant - np.dot(self.lin_coeff, orig)
 
         if abs(coef_t) < 1e-7 and abs(constant_t) < 1e-7:
             return True, 0
@@ -79,7 +82,7 @@ class Plane:
 
         if t < 0:
             return False, np.infty
-        
+
         if np.all(orig + t * dir > self.bounds[:, 0]) and np.all(orig + t * dir < self.bounds[:, 1]):
             return True, t
         else:
@@ -107,7 +110,7 @@ class Plane:
         a = b1 * c2 - b2 * c1
         b = a2 * c1 - a1 * c2
         c = a1 * b2 - b1 * a2
-        d = (- a * x1 - b * y1 - c * z1)
+        d = -a * x1 - b * y1 - c * z1
 
         lin_coeff = np.array([a, b, c])
         constant = d / norm(lin_coeff)
